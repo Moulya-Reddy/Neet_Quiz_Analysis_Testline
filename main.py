50e59e90d88f2bd77a5f41de8389e9abc78cc69b
Moulya <moulyareddy370@gmail.com>
2025-01-30 18:40:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,29 +4,31 @@ from insights_generation import generating_insights, generating_recommendations,
 from visualisation import plotting_topic_accuracy, plotting_performance_over_time
 
 def main():
-    # Load data (current and historical)
+    # Loading the datasets (current and historical)
     with open('/Users/ananthpalreddykandhala/Documents/neet_quiz_data/data/current_data.json') as f:
         current_data_of_student = json.load(f)
-    
+    # current indicates the present performance of the student
     with open('/Users/ananthpalreddykandhala/Documents/neet_quiz_data/data/historical_data.json') as f:
         historical_data_of_student = json.load(f)
+    # historical indicates the previous performance of the student
     
-    # Analyze current and historical data
+    # Analysing the datasets (current and historical data)
     current_analysis_of_student, historical_analysis_of_student = analysing_performance(current_data_of_student, historical_data_of_student)
     
-    # Generate insights and recommendations
+    # Generating the  insights and recommendations of the student 
     insights_generated = generating_insights(current_analysis_of_student, historical_analysis_of_student)
     recommendations_generated = generate_recommendations(current_analysis_of_student, historical_analysis_of_student)
     student_persona_generated = generate_student_persona(current_analysis_of_student, historical_analysis_of_student)
     
-    # Visualizations
+    # Visualisation of the performance of student 
     plotting_topic_accuracy(current_analysis_of_student, historical_analysis_of_student)
     plotting_performance_over_time(historical_analysis_of_student)
     
-    # Output insights and recommendations
+    # Printing the output of insights and recommendations
     print("Insights:", insights_generated)
     print("Recommendations:", recommendations_generated)
     print("Student Persona:", student_persona_generated)
-
+    
+# Executing the main() function
 if __name__ == "__main__":
     main()
